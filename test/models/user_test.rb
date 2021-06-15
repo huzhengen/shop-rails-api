2c3ad903cef0a184b3d79e08593d94abd4631f9f
hukai <huzhengen@163.com>
2021-06-15 14:59:13 +0800

    test users

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,28 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "valid: user with all valid things" do
+    user = User.new(email: "3@a.com", password_digest: "123456", role: 1)
+    assert user.valid?
+  end
+
+  test "invalid: user with invalid email" do
+    user = User.new(email: "444", password_digest: "123456", role: 1)
+    assert_not user.valid?
+  end
+
+  test "invalid: user with taken email" do
+    user = User.new(email: users(:one).email, password_digest: "123456", role: 1)
+    assert_not user.valid?
+  end
+
+  test "invalid: user with invalid password_digest" do
+    user = User.new(email: "4@a.com", password_digest: "", role: 1)
+    assert_not user.valid?
+  end
+
+  test "invalid: user with invalid role" do
+    user = User.new(email: "5@a.com", password_digest: "123456", role: 9)
+    assert_not user.valid?
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,8 @@ require "rails/test_help"
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors, with: :threads)
+  # https://stackoverflow.com/questions/64592173/ruby-on-rails-testing-activerecordrecordnotunique-pguniqueviolation-erro
+  # parallelize(workers: :number_of_processors, with: :threads)
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
